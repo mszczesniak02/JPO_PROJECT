37e11b1ff3d7827df7029d62d40adf0671933d67
mszczesniak02 <161470838+mszczesniak02@users.noreply.github.com>
2025-01-25 17:44:33 +0000

    TXT

--- lib/files/text_file.cpp
+++ lib/files/text_file.cpp
@@ -1,15 +1,34 @@
 #pragma once
 
 #include "file.hpp"
+#include "text_file.hpp"
 
-class TXT_File : public File {
-public:
-    explicit TXT_File(string dir) : File(std::move(dir)) {}
-
-    void analyze() override {
-        cout << "Analyzing TXT File: " << dir_ << "\n";
-        for (size_t i = 0; i < contents_.size(); ++i) {
-            cout << "Row " << i + 1 << ": " << contents_[i] << "\n";
-        }
+vector<string> TXT_File::split(string& s, string delimiter = " ") {
+    vector<string> tokens;
+    size_t pos = 0;
+    string token;
+    while ((pos = s.find(delimiter)) != string::npos) {
+        token = s.substr(0, pos);
+        tokens.push_back(token);
+        s.erase(0, pos + delimiter.length());
     }
+    tokens.push_back(s);
+
+    return tokens;
+};
+
+// Remove whitespace from the start and end of a string
+string TXT_File::remove_white(string msg) {
+    if (msg.length() < 1) return msg;
+    for (int j = 0; j <= 1; j++) {
+        int start = 0;
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg[i] == ' ' || msg[i] == '\n' || msg[i] == '\t') {
+                start++;
+            } else break;
+        }
+        msg = msg.substr(start);
+        reverse(msg.begin(), msg.end());
+    }
+    return msg;
 };
--- lib/files/text_file.hpp
+++ lib/files/text_file.hpp
@@ -1,15 +1,52 @@
 #pragma once
 
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
 #include "file.hpp"
+
+using namespace std;
+
+
+typedef struct statistics{
+    map<char,int> frequency_letters;
+    map<string,int> frequency_words;
+    
+}statistics;
 
 class TXT_File : public File {
 public:
     explicit TXT_File(string dir) : File(std::move(dir)) {}
 
     void analyze() override {
-        cout << "Analyzing TXT File: " << dir_ << "\n";
-        for (size_t i = 0; i < contents_.size(); ++i) {
-            cout << "Row " << i + 1 << ": " << contents_[i] << "\n";
+        map<char,int> frequency_letters;
+        map<string,int> frequency_words;
+        for (int i =0;i < contents_.size(); i++){
+            vector<string> line = split(contents_[i]);
+            for (auto &element : line) element = remove_white(element);
+            
+            for (auto &word : line) {
+                if (frequency_words.find(word) != frequency_words.end()) {
+                    frequency_words[word]++;
+                } else {
+                    frequency_words[word] = 1;
+                }
+                for (auto &letter : word){
+                    if (frequency_letters.find(letter) != frequency_letters.end()) {
+                        frequency_letters[letter]++;
+                    } else {
+                        frequency_letters[letter] = 1;
+                    }   
+                }
+            }
         }
+        statistics s = {frequency_letters, frequency_words};
     }
+    vector<string> split(string& s, string delimiter = " ");
+    string remove_white(string msg);
+
 };
